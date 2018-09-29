b6f04e1e24a465306acecbc2930c57d9092c53c7
Marco Conti <marcoconti83@gmail.com>
2018-09-29 08:57:30 +0200

    Math

---
+++ Foil/Utils/Math.swift
@@ -0,0 +1,63 @@
+//
+//
+// Copyright (c) 2018 Marco Conti
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+    
+
+import Foundation
+
+public func +(lhs: NSPoint, rhs: NSPoint) -> NSPoint {
+    return NSPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+}
+
+public func -(lhs: NSPoint, rhs: NSPoint) -> NSPoint {
+    return NSPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+}
+
+public func *(lhs: NSPoint, rhs: CGFloat) -> NSPoint {
+    return NSPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+}
+
+public func /(lhs: NSPoint, rhs: CGFloat) -> NSPoint {
+    return NSPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+}
+
+extension NSPoint {
+
+    public func squareDistance(to: NSPoint) -> CGFloat {
+        return ((self.x - to.x) * (self.x - to.x)) + ((self.y - to.y) * (self.y - to.y))
+    }
+    
+    public func midpoint(to: NSPoint) -> NSPoint {
+        return NSPoint(x: (to.x - self.x)/2, y: (to.y - self.y)/2) + self
+    }
+    
+    public func distance(to: NSPoint) -> CGFloat {
+        return sqrt(self.squareDistance(to: to))
+    }
+}
+
+extension NSRect {
+    
+    public var center: NSPoint {
+        return NSPoint(x: self.midX, y: self.midY)
+    }
+}
---
+++ FoilTests/MathTests.swift
@@ -0,0 +1,87 @@
+//
+//
+// Copyright (c) 2018 Marco Conti
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+    
+
+import XCTest
+import Foil
+
+class MathTests: XCTestCase {
+        
+    func testPointAddition() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0) + NSPoint(x: 4, y: -3), NSPoint(x: 4, y: -3))
+        XCTAssertEqual(NSPoint(x: 3, y: -2) + NSPoint(x: 1, y: 2), NSPoint(x: 4, y: 0))
+        XCTAssertEqual(NSPoint(x: 0, y: 0) + NSPoint(x: 0, y: 0), NSPoint(x: 0, y: 0))
+    }
+    
+    func testPointSubtraction() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0) - NSPoint(x: 4, y: -3), NSPoint(x: -4, y: 3))
+        XCTAssertEqual(NSPoint(x: 3, y: -2) - NSPoint(x: 1, y: -2), NSPoint(x: 2, y: 0))
+        XCTAssertEqual(NSPoint(x: 0, y: 0) - NSPoint(x: 0, y: 0), NSPoint(x: 0, y: 0))
+        XCTAssertEqual(NSPoint(x: 100, y: 10) - NSPoint(x: 100, y: 10), NSPoint(x: 0, y: 0))
+    }
+    
+    func testPointMultiplication() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0) * 1, NSPoint(x: 0, y: 0))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) * 1, NSPoint(x: 2, y: -2))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) * 4, NSPoint(x: 8, y: -8))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) * 0, NSPoint(x: 0, y: 0))
+        XCTAssertEqual(NSPoint(x: 3, y: 4) * -1, NSPoint(x: -3, y: -4))
+    }
+    
+    func testPointDivision() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0) / 2, NSPoint(x: 0, y: 0))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) / 2, NSPoint(x: 1, y: -1))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) / 4, NSPoint(x: 0.5, y: -0.5))
+        XCTAssertEqual(NSPoint(x: 2, y: -2) / 1, NSPoint(x: 2, y: -2))
+        XCTAssertEqual(NSPoint(x: 2, y: 4) / -1, NSPoint(x: -2, y: -4))
+        XCTAssertEqual(NSPoint(x: 3, y: 4) / 2.5, NSPoint(x: 1.2, y: 1.6))
+    }
+    
+    func testSquareDistance() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0).squareDistance(to: NSPoint(x: 1, y: 0)), 1)
+        XCTAssertEqual(NSPoint(x: 0, y: 0).squareDistance(to: NSPoint(x: 1, y: 1)), 2)
+        XCTAssertEqual(NSPoint(x: 0, y: 0).squareDistance(to: NSPoint(x: -1, y: 0)), 1)
+        XCTAssertEqual(NSPoint(x: 0, y: 0).squareDistance(to: NSPoint(x: 1, y: -1)), 2)
+        XCTAssertEqual(NSPoint(x: 2, y: 4).squareDistance(to: NSPoint(x: 0, y: 2)), 8)
+        XCTAssertEqual(NSPoint(x: 2, y: 4).squareDistance(to: NSPoint(x: 2, y: 2)), 4)
+    }
+    
+    func testDistance() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0).distance(to: NSPoint(x: 1, y: 0)), 1)
+        XCTAssertEqual(NSPoint(x: 0, y: 0).distance(to: NSPoint(x: 4, y: -3)), 5)
+        XCTAssertEqual(NSPoint(x: 0, y: 0).distance(to: NSPoint(x: -1, y: 0)), 1)
+        XCTAssertEqual(NSPoint(x: 2, y: 4).distance(to: NSPoint(x: 2, y: 2)), 2)
+    }
+    
+    func testMidPoint() {
+        XCTAssertEqual(NSPoint(x: 0, y: 0).midpoint(to: NSPoint(x: 2, y: 2)), NSPoint(x: 1, y: 1))
+        XCTAssertEqual(NSPoint(x: 0, y: 0).midpoint(to: NSPoint(x: 0, y: 0)), NSPoint(x: 0, y: 0))
+        XCTAssertEqual(NSPoint(x: 0, y: 0).midpoint(to: NSPoint(x: 2, y: -2)), NSPoint(x: 1, y: -1))
+        XCTAssertEqual(NSPoint(x: 10, y: 5).midpoint(to: NSPoint(x: 9, y: 4)), NSPoint(x: 9.5, y: 4.5))
+    }
+    
+    func testCenter() {
+        XCTAssertEqual(NSRect(x: 0, y: 0, width: 2, height: 4).center, NSPoint(x: 1, y: 2))
+        XCTAssertEqual(NSRect(x: 1, y: 1, width: 2, height: 4).center, NSPoint(x: 2, y: 3))
+    }
+}
